61d744836fc76e6343979c54fc78b263d3192f42
Kweeka <mtalbi29@yahoo.com>
2023-02-23 13:36:12 +0100

    Add validation for like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,8 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   after_save :increment_likes_counter
 
+  validates_associated :post, :user
+
   private
 
   def increment_likes_counter
