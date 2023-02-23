fe469a0d4c0487ebb4fd25a0c0b2837e68c6be95
Kweeka <mtalbi29@yahoo.com>
2023-02-23 13:36:04 +0100

    Add validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,13 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   after_save :increment_posts_counter
 
+  validates_associated :likes, :comments, :user
+
+  validates :title, length: { maximum: 250 }, allow_blank: false
+  validates :text, presence: true, length: { minimum: 100, maximum: 500 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def show_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
