950194a45c8c9d06549a86a1aedae54d6eefe9cd
Kweeka <mtalbi29@yahoo.com>
2023-02-23 13:35:54 +0100

    Add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
+  validates_associated :posts, :likes, :comments
+
+  validates :name, length: { minimum: 2, maximum: 30 }, allow_blank: false
+  validates :bio, length: { maximum: 500 }, allow_blank: true
+  validates :photo, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/i }
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def show_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
