69be7e68ec465ee85d959c6c6ec8fec8777ea7da
kweeka <mtalbi29@yahoo.com>
2023-02-24 09:15:53 +0100

    Remove validations for bio and photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
   validates_associated :posts, :likes, :comments
 
   validates :name, length: { minimum: 2, maximum: 30 }, allow_blank: false
-  validates :bio, length: { maximum: 500 }, allow_blank: true
-  validates :photo,
-            format: { with: %r{\A(http|https)://[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}i }
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def show_recent_posts
