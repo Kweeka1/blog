7c50e062faf68898cd558edca19dfd2a981dd5be
kweeka <mtalbi29@yahoo.com>
2023-03-01 19:00:55 +0100

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params.require(:user_id))
   end
+
   def show
     @post = Post.find_by(id: params.require(:post_id))
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     @user = User.find_by(id: params.require(:user_id))
     @recent_posts = @user.show_recent_posts
