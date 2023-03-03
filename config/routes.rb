5d13861197a931d476bd3189146abf28449e051a
kweeka <mtalbi29@yahoo.com>
2023-03-03 18:34:45 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/create", to: "posts#new", as: "new_post"
   post "/users/:user_id/posts/create", to: "posts#create", as: "create_post"
   get "/users/:user_id/posts/:post_id", to: "posts#show", as: "user_post"
+  post "/users/:user_id/posts/:post_id", to: "posts#create_comment", as: "create_comment"
+  get "/users/:user_id/posts/:post_id/like", to: "posts#add_like", as: "add_like"
   get "/users", to: "users#index", as: "users"
   get "/users/:user_id", to: "users#show", as: "user"
 end
