c4e6d2944b10e10c2ae5ee17ad3fa183183e714b
kweeka <mtalbi29@yahoo.com>
2023-03-03 20:49:51 +0100

    Move comments and likes logic to their own controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,29 +32,9 @@ class PostsController < ApplicationController
     end
   end
 
-  def create_comment
-    user = current_user
-    @comment = Comment.new(text: comment_params[:text], post: Post.find_by(id: params[:post_id]), user:)
-    @comment.increment_comments_counter
-    @comment.save
-    redirect_to "/users/#{user.id}/posts/#{params[:post_id]}"
-  end
-
-  def add_like
-    user = current_user
-    @like = Like.new(post: Post.find_by(id: params[:post_id]), user:)
-    @like.increment_likes_counter
-    @like.save
-    redirect_to "/users/#{user.id}/posts/#{params[:post_id]}"
-  end
-
   private
 
   def post_params
     params.require(:post).permit(:title, :text)
-  end
-
-  def comment_params
-    params.require(:comment).permit(:text)
   end
 end
