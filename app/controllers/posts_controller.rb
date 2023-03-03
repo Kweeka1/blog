53395a049792fe540430dbd7d72fedc8ab43b90e
kweeka <mtalbi29@yahoo.com>
2023-03-03 15:40:23 +0100

    Add create post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,33 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params.require(:post_id))
   end
+
+  def new
+    @post = Post.new
+    @user = params.require(:user_id)
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(
+      title: post_params[:title],
+      text: post_params[:text],
+      author_id: @user,
+      comments_counter: 0,
+      likes_counter: 0
+    )
+
+    if @post.save
+      puts 7
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
