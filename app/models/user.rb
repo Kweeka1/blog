476eceec998570ef6533230d32dba5e418812b08
Kweeka1 <mtalbi29@yahoo.com>
2023-03-07 18:57:57 +0300

    add devise configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
+  after_initialize :init
 
   validates :name, length: { minimum: 2, maximum: 30 }, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def show_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def init
+    self.posts_counter = 0
   end
 end
