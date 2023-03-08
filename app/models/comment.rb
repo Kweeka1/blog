005df615f3c77fe2c2713d0717af1fe42e95825d
kweeka <mtalbi29@yahoo.com>
2023-03-08 15:51:54 +0100

    Add decrement comments counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post
+  after_destroy :decrement_comments_counter
 
   validates :text, presence: true, length: { minimum: 2, maximum: 200 }
 
   def increment_comments_counter
     post.increment :comments_counter
+    post.save
+  end
+
+  def decrement_comments_counter
+    post.decrement :comments_counter
     post.save
   end
 end
