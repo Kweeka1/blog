ae812f7790cd0432a6f31be2f24844b64dad5f32
kweeka <mtalbi29@yahoo.com>
2023-03-08 15:25:28 +0100

    add admin? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 30 }, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def show_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
