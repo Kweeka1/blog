885de3b603e58f23b921170ac3ba9aefe2726ef5
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-03-09 13:54:45 +0100

    Add comment create action api

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,14 +1,20 @@
 class Api::CommentsController < Api::ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     @comments = Comment.all.where(author_id: params.require(:user_id), post_id: params.require(:post_id))
     render json: @comments
   end
 
+  def create
+    puts params
+    comment = Comment.new(author_id: current_user.id, post_id: params.require(:post_id), text: params[:comment][:text])
+    render json: comment
+  end
+
   private
 
-  def comment_params
+  def comment_body
     params.require(:comment).permit(:text)
   end
 end
