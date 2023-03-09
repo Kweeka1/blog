478c1571ce7506679534105bf64510ff693899fe
kweeka <mtalbi29@yahoo.com>
2023-03-09 15:38:01 +0100

    Remove commented code

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,4 @@
 class Api::CommentsController < Api::ApplicationController
-  # load_and_authorize_resource
-
   def index
     @comments = Comment.all.where(author_id: params.require(:user_id), post_id: params.require(:post_id))
     render json: @comments
